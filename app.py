bb07a66a9361e85032a7483c437ae9b4a02d9287
Aaditya Raj Singh <aaditya.singh@avantika.edu.in>
2024-09-30 22:07:28 +0530

    import mobile OTP module

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask , render_template, request
 import routes.admin as admin
 import routes.home as home
+import routes.Email_OTP as OTP_s
+import routes.Mobile_OTP as OTP_Mobile
 
 app = Flask(__name__)
 
 app.register_blueprint(home.home)
 app.register_blueprint(admin.admin)
+app.register_blueprint(OTP_s.OTP_s)
+app.register_blueprint(OTP_Mobile.OTP_Mobile)
 
 
 
